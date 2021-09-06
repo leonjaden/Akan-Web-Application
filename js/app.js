cd0176d846fd43e7ffacf3fbe25f64482aec8795
leonjaden <leon.jaden@student.moringaschool.com>
2021-09-06 03:50:24 +0300

    Add arrays containing male Akan names

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
 const akanChecker = (e) => {
   e.preventDefault();
+
+  const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+
+
+
+
+
+
+}
