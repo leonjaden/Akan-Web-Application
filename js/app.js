0da0ba2bc7b1b4f606d7775cad20ced609f79d78
leonjaden <leon.jaden@student.moringaschool.com>
2021-09-06 03:53:27 +0300

    Add variable fd to get user input from forms

--- js/app.js
+++ js/app.js
@@ -3,8 +3,10 @@ const akanChecker = (e) => {
 
   const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-  
 
+  const form = document.querySelector("#formAkans");
+
+  const fd = new FormData(form);
 
 
 
